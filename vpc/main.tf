fca4272a0ab0ef4c034d0a6a336be76106fc3dd1
kleong0221 <kenny.leong0221@yahoo.com>
2023-11-28 20:15:07 -0800

    testing github actions wf

---
+++ vpc/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+variable "instance_count" {
+  default = 5
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "random_shuffle" "az" {
+  input = ["us-west-1b", "us-west-1c"]
+}
+
+resource "aws_instance" "server" {
+  count = var.instance_count
+
+  ami           = "ami-080d1454ad4fabd12"
+  instance_type = "t2.micro"
+
+  availability_zone = element(random_shuffle.az.result, count.index)
+
+  tags = {
+    Name = "server${count.index + 1}"
+  }
+}
