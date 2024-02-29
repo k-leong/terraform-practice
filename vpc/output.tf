e0084f90529139cee96dc2070a8a9d7d25ac2885
k-leong <kenny.leong0221@yahoo.com>
2024-02-28 19:41:28 -0800

    added private subnets

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,5 +1,9 @@
-output "subnet_id" {
+output "public_subnet_ids" {
   value = [aws_subnet.public1.id, aws_subnet.public2.id]
+}
+
+output "private_subnet_ids" {
+  value = [aws_subnet.private1.id, aws_subnet.private2.id]
 }
 
 output "vpc_id" {
